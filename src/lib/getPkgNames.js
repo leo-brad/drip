50010b4d3bac1a6521d3421edfb8fc9b0e810f54
Eric Hart <>
2023-02-28 21:36:57 +0800

    [Add]

--- src/lib/getPkgNames.js
+++ src/lib/getPkgNames.js
@@ -1,9 +1,7 @@
 export default function getPkgNames(packages) {
   return packages.map((p) => {
-    const r = /^\[(\w+)\]\(([\w\-\/\.]+)\)$/;
-    if (r.test(p)) {
-      const [_, pkg] = p.match(r);
-      return pkg;
+    if (p.pkg) {
+      return p.pkg;
     }
   });
 }
